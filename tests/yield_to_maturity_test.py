9970f07260e2e1038216441bb934f44e1f5d8165
Vasanthan Kesavan <vasanthankesavan@gmail.com>
2019-11-24 18:48:38 -0500

    add additional test to yield to maturity

--- tests/yield_to_maturity_test.py
+++ tests/yield_to_maturity_test.py
@@ -6,6 +6,11 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from classes.Yield import Yield
 
 
-def test_ytm():
+def test_ytm_a():
     test = Yield(11, 1233.64, 19, 6, 1000)
     assert test.yield_to_maturity() == 8.5, "Should be 8.5%"
+
+
+def test_ytm_b():
+    test = Yield(13, 1233.64, 19, 6, 1000)
+    assert test.yield_to_maturity() == 10.2, "Should be 10.2%"
